423f1cf60820784a9645bafaf339516ca7ddadd0
jinu-jinu <kjwkjw5153@gmail.com>
2023-03-27 17:08:24 +0900

    에러찾기2

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -4,16 +4,16 @@ import { state } from '../Store/store';
 import Section1 from './Section1';
 
 const section2 = import('./Section2');
-const section3 = import('./Section3');
-// const section4 = import('./Section4');
-// const section5 = import('./Section5');
+// const section3 = import('./Section3');
+const section4 = import('./Section4');
+const section5 = import('./Section5');
 
 const Project = () => {
   const aspect = useAspect(1200, 800, 1);
   const PreloadSection2 = lazy(() => section2);
-  const PreloadSection3 = lazy(() => section3);
-  // const PreloadSection4 = lazy(() => section4);
-  // const PreloadSection5 = lazy(() => section5);
+  // const PreloadSection3 = lazy(() => section3);
+  const PreloadSection4 = lazy(() => section4);
+  const PreloadSection5 = lazy(() => section5);
 
   useLayoutEffect(() => {
     if (aspect[0] !== 0) state.aspect = aspect;
@@ -26,16 +26,16 @@ const Project = () => {
         <PreloadSection2 />
       </Suspense>
 
-      <Suspense fallback={null}>
-        <PreloadSection3 />
-      </Suspense>
-
       {/* <Suspense fallback={null}>
+        <PreloadSection3 />
+      </Suspense> */}
+
+      <Suspense fallback={null}>
         <PreloadSection4 />
       </Suspense>
       <Suspense fallback={null}>
         <PreloadSection5 />
-      </Suspense> */}
+      </Suspense>
     </group>
   );
 };
